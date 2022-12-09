4d8585ff212fbdcc613381aeffd372ce40b2ff81
Kevin Wang <lol@noemail.com>
2022-12-09 15:00:18 -0500

    start production script change

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -18,7 +18,7 @@ module.exports = {
 
   production:{
     client:'postgresql',
-    connection:process.env.DATAbASE_URL+'?ssl=no-verify',
+    connection:process.env.DATABASE_URL+'?ssl=no-verify',
     pool:{
       min:2,
       max:10

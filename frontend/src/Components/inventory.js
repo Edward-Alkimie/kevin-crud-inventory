3989c6b2ea0cf73a5c28e5345fac39e86a536529
Kevin Wang <lol@noemail.com>
2022-12-11 12:11:32 -0500

    morning save

--- frontend/src/Components/inventory.js
+++ frontend/src/Components/inventory.js
@@ -1,39 +1,48 @@
-import { application } from "express";
-import { useContext } from "react";
-import {useParams} from 'react-router-dom'
+// import { application } from "express";
+import { useContext, useEffect, useState } from "react";
+import {useNavigate, useParams} from 'react-router-dom'
 import Context from "../Contexts/Context";
-// import config from '../config';
+import config from '../config';
 
 // const ApiUrl = config[process.env.REACT_APP_NODE_ENV || "development"].apiUrl
 
-
 function Inventory(){
-    // const ApiUrl = config[process.env.REACT_APP_NODE_ENV || "development"].apiUrl
+    const ApiUrl = config[process.env.REACT_APP_NODE_ENV || "development"].apiUrl
     const {inventory} = useContext(Context);
+    const {returnValue, setReturnValue} = useState(null);
+    const navigate = useNavigate();
     const {id} = useParams();
 
-    // function DeleteInventory(){
-    //     if (true){
-    //         fetch(ApiUrl + "/inventory" ,{
-    //             method: 'DELETE',
-    //             headers:{'Content-Type': 'application/json'},
-    //             mode:'cors',
-    //             body: JSON.stringify({"id":id})
-    //         })
-    //         .then(res => console.log(res.statusText))
-    //     }
+    function DeleteInventory(){
+        if (false){
+            fetch(ApiUrl + "/inventory" ,{
+                method: 'DELETE',
+                headers:{'Content-Type': 'application/json'},
+                mode:'cors',
+                body: JSON.stringify({"id":id})
+            })
+            .then(res => setReturnValue(res.statusText))
+        }
+        else{
+            alert("you do not have the permission to delete")
+        }
+    }
 
-    // }
-    // console.log(id);
-    // console.log(inventory);
-// an if statement to double check the inventory if is [] future
+    useEffect(()=>{
+        if (returnValue){
+            alert("inventory has been deleted");
+            navigate('/');
+        }
+    }, [returnValue])
+    console.log(id);
+    console.log(inventory);
     return(
         <div>
             here is the product!!!!  
             <h1>{inventory.itemName} <br/>
                 {inventory.description} <br/>
                 {inventory.quantity}</h1>
-            <button />
+            <button onClick={DeleteInventory}>Delete</button>
         </div>
     )
 }
